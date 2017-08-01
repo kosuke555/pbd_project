f8caac88e974e2fba05d94315a1e97c9c2d031e6
kosuke555 <kosuke.horichi@gmail.com>
2017-08-02 06:43:41 +0900

    Move type definitions to typings folder

---
+++ typings/TypedArray.d.ts
@@ -0,0 +1,46 @@
+type TypedArray = Int8Array|Uint8Array|Uint8ClampedArray|Int16Array|Uint16Array|Int32Array|Uint32Array|Float32Array|Float64Array;
+
+interface Int8Array {
+    constructor: Int8ArrayConstructor;
+    __proto__: typeof Int8Array.prototype;
+}
+
+interface Uint8Array {
+    constructor: Uint8ArrayConstructor;
+    __proto__: typeof Uint8Array.prototype;
+}
+
+interface Uint8ClampedArray {
+    constructor: Uint8ClampedArrayConstructor;
+    __proto__: typeof Uint8ClampedArray.prototype;
+}
+
+interface Int16Array {
+    constructor: Int16ArrayConstructor;
+    __proto__: typeof Int16Array.prototype;
+}
+
+interface Uint16Array {
+    constructor: Uint16ArrayConstructor;
+    __proto__: typeof Uint16Array.prototype;
+}
+
+interface Int32Array {
+    constructor: Int32ArrayConstructor;
+    __proto__: typeof Int32Array.prototype;
+}
+
+interface Uint32Array {
+    constructor: Uint32ArrayConstructor;
+    __proto__: typeof Uint32Array.prototype;
+}
+
+interface Float32Array {
+    constructor: Float32ArrayConstructor;
+    __proto__: typeof Float32Array.prototype;
+}
+
+interface Float64Array {
+    constructor: Float64ArrayConstructor;
+    __proto__: typeof Float64Array.prototype;
+}
